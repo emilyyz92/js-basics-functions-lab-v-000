de78b2b5a5806584c3cf6a3772ed9b15055954f9
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-09 15:07:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 // Code your solution in this file!
 function distanceFromHqInBlocks(distance) {
+  let blocks;
   if (distance >= 42) {
-    let blocks = distance - 42;
+    blocks = distance - 42;
   } else {
-    let blocks = 42 - distance;
+    blocks = 42 - distance;
   }
   return blocks;
 }
