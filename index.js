f92533c48a3afbbc2f4fb201bdc07aa24c2e22ad
Emily Zhao <tong.zhao.92@gmail.com>
2018-08-09 15:39:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function distanceTravelledInFeet(block1, block2) {
 function calculatesFarePrice(start, destination) {
   let distance = distanceTravelledInFeet(start, destination);
   if (distance > 400 && distance < 2000) {
-    return 0.02 * distance;
+    return 0.02 * (distance - 400);
   } else if (distance > 2000 && distance < 2500) {
     return 25;
   } else if (distance < 400) {
